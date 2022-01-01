19965e7a3007a945997ab25abfb839cd2b1cab3d
Robert Fensterman <minnmass@gmail.com>
2022-01-01 14:58:43 -0600

    handle programming blocks without displays

--- PressurizedDoorManger/Program.cs
+++ PressurizedDoorManger/Program.cs
@@ -25,7 +25,13 @@ namespace IngameScript {
 		}
 
 		private void Log(string text, bool append = true) {
+			if (Me == null) {
+				return;
+			}
 			var surface = Me.GetSurface(0);
+			if (surface == null) {
+				return;
+			}
 			surface.ContentType = VRage.Game.GUI.TextPanel.ContentType.TEXT_AND_IMAGE;
 			surface.WriteText(text, append: append);
 			surface.WriteText(Environment.NewLine, append: true);
